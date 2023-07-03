2aa3020a8ef63f06ac961ad3de1ed5c13284d34d
Ricardo <ricardoclpaiva@gmail.com>
2023-07-02 22:17:02 -0300

    Implementação da tela de consulta de usuários.

--- src/app/modules/usuarios/usuarios-consulta/usuarios-consulta.component.ts
+++ src/app/modules/usuarios/usuarios-consulta/usuarios-consulta.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {Cultura} from "../../../interfaces/cultura";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {BackendService} from "../../../services/backend.service";
+import {Usuario} from "../../../interfaces/usuario";
 
 @Component({
   selector: 'app-usuarios-consulta',
   templateUrl: './usuarios-consulta.component.html',
   styleUrls: ['./usuarios-consulta.component.scss']
 })
-export class UsuariosConsultaComponent implements OnInit {
+export class UsuariosConsultaComponent implements OnInit, AfterViewInit {
   loading: boolean = true;
 
-  constructor() { }
+  listUsuarios: Usuario[] = [];
+
+  displayedColumns = ['nome_usuario', 'email_usuario', 'data_cadastro', 'id_perfil_usuario'];
+  usuariosDataSource = new MatTableDataSource<Usuario>();
+
+  // Paginador do Material Table
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  pageSizeOptions: number[] = [5, 10, 25];
+
+  // Ordenador do Material Table
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private backendService: BackendService) {}
 
   ngOnInit(): void {
+    this.backendService.getUsuarios().subscribe((listUsuarios: Usuario[]) => {
+      this.listUsuarios = listUsuarios;
+
+      this.usuariosDataSource.data = this.listUsuarios;
+
+      this.loading = false;
+    });
   }
 
+  ngAfterViewInit() {
+    this.usuariosDataSource.paginator = this.paginator;
+    this.usuariosDataSource.sort = this.sort;
+  }
 }
--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 import { UsuariosConsultaComponent } from './usuarios-consulta/usuarios-consulta.component';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [UsuariosConsultaComponent],
-  imports: [CommonModule, UsuariosRoutingModule, MatProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    UsuariosRoutingModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+  ],
 })
 export class UsuariosModule {}
